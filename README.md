4a65b53e13540092705300eefdac1e4930b917ec
Alexandre990 <alexandre9616@gmail.com>
2023-03-15 10:29:26 -0400

    first-commit

---
+++ README.md
@@ -0,0 +1,32 @@
+<h1 align="center"> Conversor de Moedas</h1>
+
+<p align="center">
+Evento exclusivo promovido pelo Rodolfo Mori no DevClub, para ensino de tecnologias WEB.
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>
+</p>
+
+<img src="https://github.com/Alexandre990/Conversor-de-Moedas/blob/master/assets/Notebook.png?raw=true" >
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- JavaScript
+- <a href="https://docs.awesomeapi.com.br/api-de-moedas">API</a>
+
+## 💻 Projeto
+
+O Conversor de moedas trás a cotação do dia, pois usa um API para dá uma melhor experiência de uso. Ele converte para três moedas físicas e uma criptomoeda.
+
+## 🔖 Layout
+
+Você pode visualizar o layout do projeto através [DESSE LINK](https://www.figma.com/file/eg239o5MNJCj9asPPwzkrQ/CodeClub-Convert-Money?node-id=25%3A104&t=2SA7AlaKoqbWOswx-0). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
+
+<p align="center">Feito com ♥ by Alexandre Oliveira</p>
---
+++ script.js
@@ -0,0 +1,74 @@
+const button = document.getElementById("convert-button");
+const select = document.getElementById("currency-select");
+const input = document.getElementById("input-real");
+
+const convertValues = async () => {
+  const inputReais = document.getElementById("input-real").value;
+  const realValueText = document.getElementById("real-value-text");
+  const currencyValueText = document.getElementById("currency-value-text");
+
+  const data = await fetch(
+    "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
+  ).then((response) => response.json());
+
+  const dolar = data.USDBRL.high;
+  const euro = data.EURBRL.high;
+  const bitcoin = data.BTCBRL.high;
+
+  realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputReais);
+
+  if (select.value === "US$ Dólar americano") {
+    currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputReais / dolar);
+  }
+
+  if (select.value === "€ Euro") {
+    currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputReais / euro);
+  }
+
+  if (select.value === "₿ Bitcoin") {
+    currencyValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BTC",
+    }).format(inputReais / bitcoin);
+  }
+};
+
+changeCurrency = () => {
+  const currencyName = document.getElementById("currency-name");
+  const currencyImg = document.getElementById("currency-img");
+
+  if (select.value === "US$ Dólar americano") {
+    currencyName.innerHTML = "Dólar americano";
+    currencyImg.src = "./assets/estados unidos.png";
+  }
+
+  if (select.value === "€ Euro") {
+    currencyName.innerHTML = "Euro";
+    currencyImg.src = "./assets/euro.png";
+  }
+
+  if (select.value === "₿ Bitcoin") {
+    currencyName.innerHTML = "Bitcoin";
+    currencyImg.src = "./assets/bitcoin.png";
+  }
+  convertValues();
+};
+
+input.addEventListener("keyup", ({ key }) => {
+  if (key === "Enter") {
+    return convertValues();
+  }
+});
+
+button.addEventListener("click", convertValues);
+
+select.addEventListener("change", changeCurrency);
